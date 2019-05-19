46295d4c685c3fefd03c92c4fd63a3b11eca1929
Romulo <rgcostabox@gmail.com>
2019-05-18 19:23:30 -0500

    route: GET and POST /api/question

--- server/controllers/question.controller.js
+++ server/controllers/question.controller.js
@@ -5,15 +5,23 @@ const questionSchema = Joi.object({
   title: Joi.string().required(),
   topic: Joi.string().required(),
   author:Joi.string().required(),
-  isPublic:Joi.boolean().required()
+  isPublic:Joi.boolean().required(),
+  status: Joi.string(),
+  answers: Joi.array()
 })
 
 
 module.exports = {
-  insert
-}
+  insert,
+  getAll
+};
 
 async function insert(question) {
+  question.status = 'open';
   question = await Joi.validate(question, questionSchema, { abortEarly: false });
   return await new Question(question).save();
+}
+
+async function getAll() {
+    return await Question.find({});
 }
