c4d5b8105554dace9abdbb7bc72147001d4d3a33
Beteab <beteablmekuria@yahoo.com>
2019-05-21 16:15:07 -0500

    Update question.component.ts

--- src/app/home/question.component.ts
+++ src/app/home/question.component.ts
@@ -1,32 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-import {QuestionService} from './home.service';
+import {QuestionService} from '../services/home.service';
 import {TokenStorage} from '../auth/token.storage';
 import {MatIconRegistry} from '@angular/material';
 
 import {ActivatedRoute} from '@angular/router';
 import {ɵDomSanitizerImpl} from '@angular/platform-browser';
 @Component({
-    selector: 'app-home',
+    selector: 'app-question',
     templateUrl: './question.component.html',
     styleUrls: ['./home.component.css']
 })
 export class QuestionComponent implements OnInit {
-    constructor(private question: QuestionService, private token: TokenStorage , private route:ActivatedRoute, private matIconRegistry: MatIconRegistry) {
+    // tslint:disable-next-line:max-line-length
+    constructor(private question: QuestionService, private token: TokenStorage , private route: ActivatedRoute, private matIconRegistry: MatIconRegistry) {
 
     }
 
     private Question: any;
-    private id:any;
+    private id: any;
+    private data: any;
 
 
     ngOnInit() {
-        this.route.paramMap.subscribe(params=>{
-          this.id =  params.get("id");
+        this.route.paramMap.subscribe(params => {
+            console.log(params);
+          this.id =  params.get('id');
         });
-        this.question.getQuestionsById(this.id).subscribe(data => {
+     this.data =   this.question.getQuestionsById(this.id);
+     this.data.subscribe(data => {
             console.log(data);
             this.Question = data;
         });
 
+    }
+
+    up(answerId) {
+        this.question.upVote( this.id  , answerId).subscribe(data => {
+         this.Question =  data;
+        });
+
+
+    }
+    down(answerId) {
+        this.question.downVote(this.id , answerId).subscribe(data => {
+            this.Question = data;
+        });
     }
 }
