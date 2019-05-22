2bb9ce821bb0da40b17a7f9f2e7cebf04da4b98c
Beteab <beteablmekuria@yahoo.com>
2019-05-21 20:37:38 -0500

    Update profile.route.js

--- server/routes/profile.route.js
+++ server/routes/profile.route.js
@@ -10,8 +10,30 @@ router.use(passport.authenticate('jwt', { session: false }))
 
 router.route('/:userId/questions')
     .get(asyncHandler(getAllQuestions));
-
+router.route('/:userId/answers')
+    .get(asyncHandler(getAllAnswers));
+router.route('/:userId/questions/:questionId')
+    .delete(asyncHandler(deleteQuestion));
+router.route('/:userId/answers/:answerId')
+    .delete(asyncHandler(deleteAnswer))
 async function getAllQuestions(req, res) {
+
     const questions = await profileCtrl.getAllQuestions(req.params.userId);
     res.status(200).json(questions);
 }
+async function getAllAnswers(req,res) {
+    const answers = await profileCtrl.getAllAnswers(req.params.userId);
+    res.status(200).json(answers);
+}
+async function deleteQuestion(req,res) {
+    console.log(req.params.userId);
+    const deleted = await profileCtrl.deleteQuestion(req.params.userId ,req.params.questionId);
+    res.status(200).json({success:1});
+    
+}
+async function deleteAnswer(req,res) {
+    await profileCtrl.deleteAnswer(req.params.answerId);
+    res.status(200).json({success:1});
+    
+}
+
